d9d4b82e9a19f0c19ecb2b27959837121516c322
TanguyHsrt <tanguyherserant@gmail.com>
2019-10-10 21:48:05 +0200

    Reste à faire les formules

--- project.R
+++ project.R
@@ -10,3 +10,123 @@ jeu2 <- simdataset(n = 500, Pi = jeu2.Q$Pi, Mu = jeu2.Q$Mu, S = jeu2.Q$S)
 #jeu 3
 jeu3.Q <- MixSim(MaxOmega = 0.10, BarOmega = 0.05, K = 3, p = 2, sph = FALSE)
 jeu3 <- simdataset(n = 500, Pi = jeu3.Q$Pi, Mu = jeu3.Q$Mu, S = jeu3.Q$S)
+
+#Fonctions classif
+classification1 = function(x,Mu,sigma){
+  #Dans notre cas : lnP(ci ) = lnP(cj )
+  # donc : g_i(x)-|x-mu_i|^2
+  return(-det(x-Mu)^2)
+}
+
+classification2 = function(x,Mu,sigma){
+  return(t(Mu)%*%inv(sigma)*x + (log(1/2)-1/2*t(Mu)%*%inv(sigma)%*%Mu))
+}
+
+classification3 <- function(x,Mu,sigma){
+  return (-1/2*t(x-Mu)%*%solve(sigma)%*%(x-Mu) - 1/2 *log(det(sigma)) +log(1/2))
+}
+
+maxClassif = function(jeu,classif,x){ # Jeu = JDD / classif (1, 2 ou 3) choix de la fonction classif
+  
+    if(classif == 1){
+      print(jeu$Mu)
+      a = classification1(x,jeu$Mu[1,], jeu$S[,,1])
+      b = classification1(x,jeu$Mu[2,], jeu$S[,,2])
+      if (a>b) {
+        return(1)
+      }else{
+        return(2)
+      }
+      
+    } else if (classif == 2 ){
+      a = classification2(x,jeu$Mu[1,], jeu$S[,,1])
+      b = classification2(x,jeu$Mu[2,], jeu$S[,,2])
+    } else if (classif == 3){
+      a = classification3(x,jeu$Mu[1,], jeu$S[,,1])
+      b = classification3(x,jeu$Mu[2,], jeu$S[,,2])
+    }
+  
+  
+  if (a>b && a>c) {
+    return(1)
+  }else if(b>a && b>c){
+    return(2)
+  }else if(c>a && c>b){
+    return(3)
+  }
+}
+
+
+#id
+id <- matrix(0,nrow=500,ncol=9) # col = 9 car on aura 9 tests 
+# col 1 : Jeu1 + classif1
+# col 2 : Jeu1 + classif2
+# col 3 : Jeu1 + classif3
+# col 4 : Jeu2 + classif1
+# col 5 : Jeu2 + classif2
+# col 6 : Jeu2 + classif3
+# col 7 : Jeu3 + classif1
+# col 8 : Jeu3 + classif2
+# col 9 : Jeu3 + classif3
+
+for (i in 1:500) {
+  id[i,1] = maxClassif(jeu1.Q, 1, jeu1$X[i,])
+  id[i,2] = maxClassif(jeu1.Q, 2, jeu1$X[i,])
+  id[i,3] = maxClassif(jeu1.Q, 3, jeu1$X[i,])
+  id[i,4] = maxClassif(jeu2.Q, 1, jeu2$X[i,])
+  id[i,5] = maxClassif(jeu2.Q, 2, jeu2$X[i,])
+  id[i,6] = maxClassif(jeu2.Q, 3, jeu2$X[i,])
+  id[i,7] = maxClassif(jeu3.Q, 1, jeu3$X[i,])
+  id[i,8] = maxClassif(jeu3.Q, 2, jeu3$X[i,])
+  id[i,9] = maxClassif(jeu3.Q, 3, jeu3$X[i,])
+}
+
+colors <- c("red", "green", "blue")
+
+plotRealAndPredict = function(jeu, predicts){
+  par(mfrow = c(3,2),mar = c(0.1, 0.1, 0.1, 0.1))
+  # Premiere classif
+  plot(jeu$X, col = colors[jeu$id], pch = 19, cex = 0.8, 
+       xlab = "", ylab = "", axes = T, main = "Classif1 real")
+  plot(jeu$X, col = colors[predict[,1]], pch = 19, cex = 0.8, 
+       xlab = "", ylab = "", axes = T, main = "Classif1 predict")
+  # 2eme 
+  plot(jeu$X, col = colors[jeu$id], pch = 19, cex = 0.8, 
+       xlab = "", ylab = "", axes = T, main = "Classif2 real")
+  plot(jeu$X, col = colors[predict[,2]], pch = 19, cex = 0.8, 
+       xlab = "", ylab = "", axes = T, main = "Classif2 predict")
+  
+  # 3eme
+  plot(jeu$X, col = colors[jeu$id], pch = 19, cex = 0.8, 
+       xlab = "", ylab = "", axes = T, main = "Classif3 real")
+  plot(jeu$X, col = colors[predict[,3]], pch = 19, cex = 0.8, 
+       xlab = "", ylab = "", axes = T, main = "Classif3 predict")
+}
+
+plotRealAndPredict(jeu1,id[,c(1:3)])
+
+plotRealAndPredict(jeu2,id[,c(4:6)])
+
+plotRealAndPredict(jeu3,id[,c(7:9)])
+
+
+# par(mfrow = c(1,2),mar = c(0.1, 0.1, 0.1, 0.1))
+# 
+# plot(jeu1$X, col = colors[jeu1$id], pch = 19, cex = 0.8, 
+#      xlab = "", ylab = "", axes = FALSE)
+# par(mar = c(0.1, 0.1, 0.1, 0.1))
+# plot(jeu1$X, col = colors[id], pch = 19, cex = 0.8, 
+#      xlab = "", ylab = "", axes = FALSE)
+# table(jeu1$id,id)
+
+
+# maxClassif = function(x,Mu,sigma){
+#   a = classification3(x,jeu1.Q$Mu[1,], jeu1.Q$S[,,1])
+#   b = classification3(x,jeu1.Q$Mu[2,], jeu1.Q$S[,,2])
+#   if (a>b) {
+#     return(1)
+#   }else{
+#     return(2)
+#   }
+# }
+#id = apply(jeu1$X,FUN = maxClassif,MARGIN = F)
